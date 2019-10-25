4d32aa5ae9bfa1e7b772c6af337efacad7585566
Johan Kneubuhler <johan.m.k6@gmail.com>
2019-10-25 11:35:21 -0300

    saved

--- php/login.js
+++ php/login.js
@@ -1,9 +1,34 @@
 const domain = 'http://127.0.0.1/';
 
-$('#submit').on("click", function() {
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET", domain + 'session.php?username=' + $('#login').val() + '&password=' + $('#pass').val() , false);
-    Httpreq.send();
-    $(location).attr('href', domain);
-});
+$(document).on('click', '#submit', function () {
+    console.log('entrou');
+    //var Httpreq = new XMLHttpRequest(); // a new request
+    //Httpreq.open("GET", domain + 'session.php?username=' + $('#login').val() + '&password=' + $('#pass').val(), false);
+    //Httpreq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    //Httpreq.send();
 
+    var username = $('#login').val();
+    var pass = $('#pass').val();
+
+    var request = new XMLHttpRequest();
+    var url = domain + "session.php";
+    var params = "username=" + username + "&password=" + pass;
+
+    request.open('GET', url + '?' + params, true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var response = JSON.parse(request.response);
+                if (response.result) {
+                    alert("funfou");
+                } else {
+                    alert("não funfou");
+                }
+            }
+        }
+    };
+    request.send(null);
+
+    //$(location).attr('href', domain);
+});
--- php/session.php
+++ php/session.php
@@ -1,8 +1,10 @@
 <?php
+    header("Content-Type: application/json; charset=UTF-8");
+    
     session_start();
 
-    if ( ! empty( $_GET ) ) {
-        if ( isset( $_GET['username'] ) && isset( $_GET['password'] ) ) {
+    if ( ! empty( $_POST ) ) {
+        if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
             // Getting submitted user data from database
             $db_host = '127.0.0.1:3306';
             $db_user = 'root';
@@ -11,14 +13,18 @@
 
             $con = new mysqli($db_host, $db_user, $db_pass, $db_name);
             $stmt = $con->prepare("SELECT * FROM users WHERE name = ?");
-            $stmt->bind_param('s', $_GET['username']);
+            $stmt->bind_param('s', $_POST['username']);
             $stmt->execute();
             $result = $stmt->get_result();
             $user = $result->fetch_object();
             // Verify user pass and set $_SESSION
-            if ( password_verify( $_GET['password'], $user->pass ) ) {
-                $_SESSION['id'] = $user->id;
+            if ( password_verify( $_POST['password'], $user->pass ) ) {
                 $_SESSION['user'] = $user;
+                echo json_encode($user);
+            }else{
+                echo json_encode(
+                    array("body" => array(), "count" => 0);
+                );
             }
         }
     }
